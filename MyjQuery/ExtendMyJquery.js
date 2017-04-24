b64aca73855d469c3ba0b34125c7527feab0a203
ChallengerCY <git@790597004@qq.com>
2017-04-24 17:36:38 +0800

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.  On branch master  Your branch is up-to-date with 'origin/master'.
     Changes to be committed:
    	new file:   MyjQuery/ExtendMyJquery.js
    new file:   MyjQuery/jquery-3.2.0.js
    	new file:   MyjQuery/myjQuery.html
    	new file:   MyjQuery/myjQuery.js

---
+++ MyjQuery/ExtendMyJquery.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Administrator on 2017/4/6 0006.
+ */
+$(document).ready(function(){
+    $.myjQuery();
+    $("div").myjQuery();
+
+})
---
+++ MyjQuery/myjQuery.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2017/4/6 0006.
+ */
+$.myjQuery=function(){
+    alert("this is my jQuery");
+}
+
+$.fn.myjQuery=function(){
+    $(this).text("hello");
+}
